ad68e4d6ed4f7257fc1d471c6cefaff596903463
MishaAprelskaya <mikhal.aprelskaya@gmail.com>
2021-02-21 15:09:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def push_aroma(event_click):
+    if event_click == True:
+        return push_aroma
+
