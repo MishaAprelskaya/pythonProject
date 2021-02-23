af03151e6f3840e55f905b26c2e88ac724948977
MishaAprelskaya <mikhal.aprelskaya@gmail.com>
2021-02-23 18:08:53 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-<<<<<<< HEAD
 
-def push_aroma(event_click):
-    if event_click == True:
-        return push_aroma
-=======
 class Aromat:
     def __int__(self, item_aromat):
-        self.item_aromat=list(item.aromat):
-        def __getattr__(self.i):
-            returns self.item_aromat[i]
+        self.item_aromat = list(item.aromat)
+        def __getattr__(self, i):
+            return self.item_aromat[i]
 
 
 class Diffuser:
      def __init__(self):
-     self.work=0
-     def diffuser(self.item_aromat.room):
-     for i in room:
+      self.work = 0
+
+     def diffuser(self, item_aromat, room):
+       for i in room:
          i.take(item_aromat)
-         self.work=1
+         self.work += 1
 
 class Room:
     def __init__(self):
-    self.aroma_room=[]
-    def take(self.item_aromat):
-    self.aroma_room.append(item_aromat)
+      self.aroma_room=[]
 
->>>>>>> f8fac15 (Initial commit)
+    def take(self, item_aromat):
+      self.aroma_room.append(item_aromat)
+
 
